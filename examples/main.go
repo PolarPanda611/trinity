dad4a4f9d0b9d56174db1cfcd6ea3aba6999248e
daniel <daniel.gj.tan@gmail.com>
2019-12-09 16:12:24 +0800

    update examples

--- examples/main.go
+++ examples/main.go
@@ -1,9 +1,16 @@
 package main
 
-import "github.com/PolarPanda611/trinity"
+import (
+	"flag"
+
+	"github.com/PolarPanda611/trinity"
+)
+
+var runmode = flag.String("runmode", "release", "running mode options: debug , test , release ")
 
 func main() {
-	t := trinity.New("local")
+	flag.Parse()
+	t := trinity.New(*runmode)
 	// rg := t.NewAPIGroup("/api/v1")
 	// trinity.NewAPIInGroup(rg, "exchange_rate_types", servicev1.ExchangeRateTypeViewSet, []string{"Retrieve", "List", "Create", "Update", "Delete"})
 	t.Serve()
