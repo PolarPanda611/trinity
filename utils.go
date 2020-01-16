650f0f6d8ad78b82dcaedcf38cf59169deaa3cd8
daniel <daniel.gj.tan@gmail.com>
2020-01-16 10:36:13 +0800

    add preload dbbackend

--- utils.go
+++ utils.go
@@ -244,7 +244,12 @@ func QueryByPreload(PreloadList map[string]func(db *gorm.DB) *gorm.DB) func(db *
 	return func(db *gorm.DB) *gorm.DB {
 		if len(PreloadList) > 0 {
 			for k, v := range PreloadList {
-				db = db.Preload(k, v)
+				if v == nil {
+					db = db.Preload(k)
+				} else {
+					db = db.Preload(k, v)
+				}
+
 			}
 		}
 		return db
