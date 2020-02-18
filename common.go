88747351708a48b39d4667a0b00bda56572068b9
daniel <daniel.gj.tan@gmail.com>
2020-02-18 21:47:48 +0800

    update common

--- common.go
+++ common.go
@@ -29,6 +29,7 @@ type Simpmodel struct {
 type Logmodel struct {
 	ID           int64      `json:"key,string"  gorm:"primary_key;AUTO_INCREMENT:false"`
 	CreatedTime  *time.Time `json:"created_time" `
+	CreateUser   *User      `json:"create_user"`
 	CreateUserID int64      `json:"create_user_id,string" `
 }
 
@@ -36,10 +37,13 @@ type Logmodel struct {
 type Viewmodel struct {
 	ID           int64      `json:"key,string"  gorm:"primary_key;AUTO_INCREMENT:false"`
 	CreatedTime  *time.Time `json:"created_time" `
+	CreateUser   *User      `json:"create_user"`
 	CreateUserID int64      `json:"create_user_id,string"`
 	UpdatedTime  *time.Time `json:"updated_time" `
+	UpdateUser   *User      `json:"update_user"`
 	UpdateUserID int64      `json:"update_user_id,string"`
 	DeletedTime  *time.Time `json:"deleted_time" `
+	DeleteUser   *User      `json:"delete_user"`
 	DeleteUserID int64      `json:"delete_user_id,string"`
 }
 
@@ -47,9 +51,12 @@ type Viewmodel struct {
 type AutoModel struct {
 	ID           int64      `json:"key,string"  gorm:"primary_key;"`
 	CreatedTime  *time.Time `json:"created_time" `
+	CreateUser   *User      `json:"create_user"`
 	CreateUserID int64      `json:"create_user_id,string"`
 	UpdatedTime  *time.Time `json:"updated_time" `
+	UpdateUser   *User      `json:"update_user"`
 	UpdateUserID int64      `json:"update_user_id,string"`
 	DeletedTime  *time.Time `json:"deleted_time" `
+	DeleteUser   *User      `json:"delete_user"`
 	DeleteUserID int64      `json:"delete_user_id,string"`
 }
